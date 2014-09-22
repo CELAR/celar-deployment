6ef1f04258e3de4d83ee7d39627c9cf64daaa329
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2014-09-22 18:07:07 +0200

    Minor.

--- orchestrator/jcatascopia-server.sh
+++ orchestrator/jcatascopia-server.sh
@@ -3,19 +3,19 @@
 TOMCAT_VERSION=7.0.55
 TOMCAT_DIR=/usr/share
 
-yum -y update
 yum install -y wget
 
 #install java
 yum -y install java-1.7.0-openjdk
 
 #install cassandra
-touch /etc/yum.repos.d/datastax.repo
-echo "[datastax]" >> /etc/yum.repos.d/datastax.repo
-echo "name= DataStax Repo for Apache Cassandra" >> /etc/yum.repos.d/datastax.repo
-echo "baseurl=http://rpm.datastax.com/community" >> /etc/yum.repos.d/datastax.repo
-echo "enabled=1" >> /etc/yum.repos.d/datastax.repo
-echo "gpgcheck=0" >> /etc/yum.repos.d/datastax.repo
+cat > /etc/yum.repos.d/datastax.repo <<EOF
+[datastax]
+name= DataStax Repo for Apache Cassandra
+baseurl=http://rpm.datastax.com/community
+enabled=1
+gpgcheck=0
+EOF
 yum -y install dsc20
 
 #start cassandra
@@ -23,8 +23,7 @@ cassandra -f &
 
 #download JCatascopia-Server and install it
 #change location of rpm package to celar repo
-wget http://109.231.122.22:8080/downloads/JCatascopia-Server-0.0.1-1.noarch.rpm
-rpm -Uvh JCatascopia-Server-0.0.1-1.noarch.rpm 
+rpm -Uvh http://109.231.122.22:8080/downloads/JCatascopia-Server-0.0.1-1.noarch.rpm
 /etc/init.d/JCatascopia-Server start
 
 #download and instantiate tomcat
@@ -37,5 +36,4 @@ $TOMCAT_DIR/tomcat/bin/startup.sh
 
 #download and install JCatascopia-Web
 #default location for tomcat based on rpm is /usr/share/tomcat/...
-wget http://109.231.122.22:8080/downloads/JCatascopia-Web-0.0.1-1.noarch.rpm
-rpm -Uvh JCatascopia-Web-0.0.1-1.noarch.rpm
+rpm -Uvh http://109.231.122.22:8080/downloads/JCatascopia-Web-0.0.1-1.noarch.rpm
