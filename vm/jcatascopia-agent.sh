d1f92a066192b4c0b5c0578816dcda705816e944
dtrihinas <trihinas@cs.ucy.ac.cy>
2014-09-24 15:13:07 +0300

    Update jcatascopia-agent.sh
    JCatascopia-Agent tarball is now downloaded from CELAR repo

--- vm/jcatascopia-agent.sh
+++ vm/jcatascopia-agent.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
+#must be set at runtime to JCatascopia-Server IP, default set to localhost
 SERVER_IP=localhost
-JC_VERSION=0.0.1
+
+CELAR_REPO=http://snf-175960.vm.okeanos.grnet.gr
+JC_VERSION=LATEST
+JC_ARTIFACT=JCatascopia-Agent
+JC_GROUP=eu.celarcloud.cloud-ms
+JC_TYPE=tar.gz
 
 DISTRO=$(eval cat /etc/*release)
-
 if [[ "$DISTRO" == *Ubuntu* ]]; then
-	apt-get update -y
-	#download and install java
-	apt-get install -y openjdk-7-jre-headless
+        apt-get update -y
+        #download and install java
+        apt-get install -y openjdk-7-jre-headless
 fi
 
 if [[ "$DISTRO" == *CentOS* ]]; then
-	yum -y update
-	yum install -y wget
-	#download and install java
-	yum -y install java-1.7.0-openjdk
+        yum -y update
+        yum install -y wget
+        #download and install java
+        yum -y install java-1.7.0-openjdk
 fi
 
 #download,install and start jcatascopia agent...
-if [ ! -d JCatascopia-Agent-$JC_VERSION-SNAPSHOT ]; then
-  wget http://109.231.122.22:8080/downloads/JCatascopia-Agent-$JC_VERSION-SNAPSHOT.tar.gz
-  tar xvfz JCatascopia-Agent-$JC_VERSION-SNAPSHOT.tar.gz
-  eval "sed -i 's/server_ip=.*/server_ip=$SERVER_IP/g' JCatascopia-Agent-$JC_VERSION-SNAPSHOT/JCatascopiaAgentDir/resources/agent.properties"
-  cd JCatascopia-Agent-$JC_VERSION-SNAPSHOT
-  ./installer.sh
-  cd ..
-fi
+URL="$CELAR_REPO/nexus/service/local/artifact/maven/redirect?r=snapshots&g=$JC_GROUP&a=$JC_ARTIFACT&v=$JC_VERSION&p=$JC_TYPE"
+echo $URL
+wget -O JCatascopia-Agent.tar.gz $URL
+tar xvfz JCatascopia-Agent.tar.gz
+
+eval "sed -i 's/server_ip=.*/server_ip=$SERVER_IP/g' JCatascopia-Agent-*/JCatascopiaAgentDir/resources/agent.properties"
+cd JCatascopia-Agent-*
+./installer.sh
+cd ..
 /etc/init.d/JCatascopia-Agent restart
