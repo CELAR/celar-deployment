8d7f73730eb77a4d203755fbc2a192aa04a9b616
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-10-02 13:16:19 +0200

    Update install.sh
    Moved addition of iptables rules as the last task in the install

--- orchestrator/install.sh
+++ orchestrator/install.sh
@@ -12,10 +12,10 @@ GITHUB_BASEURL=${GITHUB_PROJECTURL}/${GITHUB_BRABCH}
 yum install -y curl
 
 SCRIPTS="install-base-deps.sh
-orchestrator/add-iptables-rules.sh
 orchestrator/slipstream-orch-deps.sh
 orchestrator/jcatascopia-server.sh
-orchestrator/dmm-install.sh"
+orchestrator/dmm-install.sh
+orchestrator/add-iptables-rules.sh"
 
 for SCRIPT in $SCRIPTS; do
     echo "::: Downloading and launching $SCRIPT"
