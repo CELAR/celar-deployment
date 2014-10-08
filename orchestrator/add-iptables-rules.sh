b10d5e5b182d3cea6a08adcb3a11879ccb9d238c
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-10-08 10:24:40 +0200

    Update add-iptables-rules.sh
    Added ESTABLISHED in rule state, and added OUTPUT in rules for JCatascopia

--- orchestrator/add-iptables-rules.sh
+++ orchestrator/add-iptables-rules.sh
@@ -6,21 +6,24 @@ iptables -D FORWARD -j REJECT --reject-with icmp-host-prohibited
 #add custom rules for CELAR MODULES
 
 #rule for accessing JCatascopia
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 8080 -j ACCEPT
 
 #rule for accessing MELA Data Service
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8180 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 8180 -j ACCEPT 
 
 #rule for accessing MELA Analysis Service
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8181 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 8181 -j ACCEPT 
 
 #rule for accessing rSYBL
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8280 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 8280 -j ACCEPT 
 
 #rule for JCatascopia Agents connecting to Server
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 4242 -j ACCEPT 
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 4243 -j ACCEPT 
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 4245 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4242 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4243 -j ACCEPT 
+iptables -A INPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4245 -j ACCEPT 
+iptables -A OUTPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4242 -j ACCEPT 
+iptables -A OUTPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4243 -j ACCEPT 
+iptables -A OUTPUT -m state --state NEW,ESTABLISHED -m tcp -p tcp --dport 4245 -j ACCEPT 
 
 #add the last two rejects statements back
 #in testing, if I add rules after this reject is mentioned, the rules are ignored
