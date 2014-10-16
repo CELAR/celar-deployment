097ab15023f900cd7ed4c4154a8afb4034b9500d
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2014-10-16 15:51:25 +0200

    Install paramiko with pycrypto.

--- orchestrator/slipstream-orch-deps.sh
+++ orchestrator/slipstream-orch-deps.sh
@@ -10,4 +10,11 @@ function install_pkgs() {
 	$cmd -y install $@
 }
 
-install_pkgs python python-pip gcc
+install_pkgs python python-pip gcc python-devel
+
+PYPI_PARAMIKO_VER=1.9.0
+PYPI_SCPCLIENT_VER=0.4
+# python-crypto clashes with Crypto installed as dependency with paramiko
+yum remove -y python-crypto || true
+pip install -Iv paramiko==$PYPI_PARAMIKO_VER
+pip install -Iv scpclient==$PYPI_SCPCLIENT_VER
