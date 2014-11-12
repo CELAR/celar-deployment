6bbcf735e573460afa85e76791c33ea7f84691d7
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2014-11-12 11:14:35 +0100

    Added export of env.vars to support deployments w/o of SlipStream

--- server/install.sh
+++ server/install.sh
@@ -2,6 +2,17 @@
 
 set -exo pipefail
 
+# The deployment assumes usage of SlipStream and its messaging via ss-get CLI.
+# When deploying outside of SlipStream, uncomment and, if required, 
+# update the following exports.  The environment vairables are used 
+# in other scripts throughout the deployment process.
+#export CELAR_REPO_KIND=releases
+#export CELAR_REPO_BASEURL=http://snf-175960.vm.okeanos.grnet.gr/yum
+#export GITHUB_BRANCH=master
+#export GITHUB_PROJECTURL=https://raw.githubusercontent.com/CELAR/celar-deployment
+#export SS_REPO_KIND=Releases
+#export CONNECTORS="fco okeanos"
+
 if [ -z "${GITHUB_BRANCH}" ]; then
    export GITHUB_BRANCH=$(ss-get github-branch)
 fi 
